b18f99819eb1f243f194de5da7d837a272b24ef7
Iain Wood <mayasky76@gmail.com>
2020-01-18 14:59:28 +0000

    added tiptap

--- src/components/Editors/TextEditor.vue
+++ src/components/Editors/TextEditor.vue
@@ -41,7 +41,144 @@
 
       <v-row>
         <v-col cols="12">
+          
+           <div class="editor">
+    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+      <div class="menubar">
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
+        >
+          <v-icon>format_bold</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
+           <v-icon>format_italic</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          <v-icon>format_strikethrough</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+        <v-icon>format_underline</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+         <v-icon>code</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+         P
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+          @click="commands.heading({ level: 1 })"
+        >
+          H1
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H2
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H3
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+       <v-icon>format_list_bulleted</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          <v-icon>format_list_numbered</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          <v-icon>format_quote</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code_block() }"
+          @click="commands.code_block"
+        >
+         <v-icon>code</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          @click="commands.horizontal_rule"
+        >
+          _
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          @click="commands.undo"
+        >
+          <v-icon>undo</v-icon>
+        </v-btn>
+
+        <v-btn
+          class="menubar__button"
+          @click="commands.redo"
+        >
+          <v-icon>redo</v-icon>
+        </v-btn>
+
+      </div>
+    </editor-menu-bar>
+
+    <editor-content class="editor__content" :editor="editor" />
+    </div>
+    <!--
           <textarea class="editor" v-model="BodyText" placeholder="Write here" @keyup="SaveMyText"></textarea>
+          -->
         </v-col>
       </v-row>
         
@@ -51,18 +188,85 @@
 </template>
 
 <script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions'
+
 export default {
+    components: {
+    EditorContent,
+     EditorMenuBar,
+  },
   props:["myEl"],
   data() {
     return {
       dialog: false,
-      BodyText: "",
-showPrefs : false,
+      BodyText: "", 
+      showPrefs : false,
+ editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        content: `
+          <h2>
+            Hi there,
+          </h2>
+          <p>
+            this is a very <em>basic</em> example of tiptap.
+          </p>
+          <pre><code>body { display: none; }</code></pre>
+          <ul>
+            <li>
+              A regular list
+            </li>
+            <li>
+              With regular items
+            </li>
+          </ul>
+          <blockquote>
+            It's amazing 👏
+            <br />
+            – mom
+          </blockquote>
+        `,
+      }),
 
     };
   },
   mounted() {
-
       console.log("mounting editor", this.myEl.uuid);
       let searchObj = {};
       searchObj.uuid = this.myEl.uuid;
@@ -107,7 +311,11 @@ showPrefs : false,
       this.$root.ProjectState.SelectedCard.name = this.myEl.name;
       this.$root.SaveProjectData();
     }
-  }
+  },
+    beforeDestroy() {
+    // Always destroy your editor instance when it's no longer needed
+    this.editor.destroy()
+  },
 };
 </script>
 
