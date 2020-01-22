d6f93595b8b4698ce2200722ab5ec22e8c860cab
Iain Wood <iain.wood@open.ac.uk>
2020-01-22 17:18:52 +0000

    update

---
+++ dist/precache-manifest.888127a84e92facdbddd9f20e7faafbd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f237e104ac69e24f77ed",
+    "url": "css/app.d351af38.css"
+  },
+  {
+    "revision": "01ce27394e8fdc1c952e",
+    "url": "css/chunk-vendors.25348632.css"
+  },
+  {
+    "revision": "581712376fdde74ed2f54ed5510a3e27",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "4cb0587189279da5476b316adecf0cd4",
+    "url": "images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "9a085ff43ba878b5f1f0bd7b25d3191e",
+    "url": "images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "137b702c50e2b2e8b61a41f48ddd6860",
+    "url": "images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "9097b194de473a1f8d90b0696f696d8b",
+    "url": "images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "b38a91f61d48a5225e64763027fa7fae",
+    "url": "images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "b86c029c0f2b5b01e9717d44df234163",
+    "url": "images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "aa870f210725fdd0fe96e44ddcdff78a",
+    "url": "images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "f9779fe1fe6cfea0bb7431c430cf5eb8",
+    "url": "images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "img/images.txt"
+  },
+  {
+    "revision": "2fb772c94e8723426f73a82fbabbac00",
+    "url": "index.html"
+  },
+  {
+    "revision": "f237e104ac69e24f77ed",
+    "url": "js/app.c29829d9.js"
+  },
+  {
+    "revision": "01ce27394e8fdc1c952e",
+    "url": "js/chunk-vendors.9cd4231e.js"
+  },
+  {
+    "revision": "1815c6ed113b27ff7469cdc5429d2fe9",
+    "url": "manifest-lv.json"
+  },
+  {
+    "revision": "ffda5d3bb49a3b955f57b5e2fb5221ba",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
