524199a588ce7dbc42c5201c38dcdf20a386abab
Iain Wood <mayasky76@gmail.com>
2020-01-23 20:21:48 +0000

    hu

---
+++ dist/precache-manifest.3004512aad3d49743566e97f0ccfec53.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c919b6c41cb46c366e",
+    "url": "css/app.7c3f4eea.css"
+  },
+  {
+    "revision": "8fe58d14d3cf57f614db",
+    "url": "css/chunk-vendors.28c46860.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "img/images.txt"
+  },
+  {
+    "revision": "ebab45746fe7b3e1187da4dff7350d72",
+    "url": "index.html"
+  },
+  {
+    "revision": "24c919b6c41cb46c366e",
+    "url": "js/app.756e7505.js"
+  },
+  {
+    "revision": "8fe58d14d3cf57f614db",
+    "url": "js/chunk-vendors.7c4cea45.js"
+  },
+  {
+    "revision": "ffda5d3bb49a3b955f57b5e2fb5221ba",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
